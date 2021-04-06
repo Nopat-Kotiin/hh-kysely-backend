516a44b41e6fe8619057bd4514d369ef2f96e99c
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-04-06 14:34:38 +0300

    removed restController

--- src/main/java/fi/hh/swd22/HHkysely/web/KyselyController.java
+++ src/main/java/fi/hh/swd22/HHkysely/web/KyselyController.java
@@ -3,18 +3,19 @@ package fi.hh.swd22.HHkysely.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.hh.swd22.HHkysely.domain.Kysely;
 import fi.hh.swd22.HHkysely.domain.KyselyRepository;
 import fi.hh.swd22.HHkysely.domain.KysymysRepository;
 
-@RestController
+@Controller
 @CrossOrigin("*")
 public class KyselyController {
 
@@ -25,7 +26,7 @@ public class KyselyController {
     private KysymysRepository kysymysRepository;
 
     @GetMapping("/kyselyt")
-    public List<Kysely> getKyselyt() {
+    public @ResponseBody List<Kysely> getKyselyt() {
         return (List<Kysely>) kyselyRepository.findAll();
     }
 
