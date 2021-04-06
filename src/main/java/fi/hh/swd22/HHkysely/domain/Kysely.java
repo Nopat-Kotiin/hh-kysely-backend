1a9e874003aecda2121a632c92bb4cf5c12aea2f
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-04-06 13:19:49 +0300

    column join

--- src/main/java/fi/hh/swd22/HHkysely/domain/Kysely.java
+++ src/main/java/fi/hh/swd22/HHkysely/domain/Kysely.java
@@ -1,11 +1,15 @@
 package fi.hh.swd22.HHkysely.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
@@ -16,6 +20,10 @@ public class Kysely {
 	private long kyselyId;
 	
 	private String nimi;
+
+	@JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "kysely")
+	private List<Kysymys> kysymykset;
 		
 	public Kysely() {}
 	
