b5c2471320b73b6c66eaf6e67a0213956befdbcc
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-04-29 12:07:39 +0300

    test survey changed

--- src/main/java/fi/hh/swd22/HHkysely/HhKyselyApplication.java
+++ src/main/java/fi/hh/swd22/HHkysely/HhKyselyApplication.java
@@ -9,6 +9,7 @@ import fi.hh.swd22.HHkysely.domain.Survey;
 import fi.hh.swd22.HHkysely.domain.SurveyRepository;
 import fi.hh.swd22.HHkysely.domain.TextQuestion;
 import fi.hh.swd22.HHkysely.domain.RadioQuestion;
+import fi.hh.swd22.HHkysely.domain.CheckboxQuestion;
 import fi.hh.swd22.HHkysely.domain.QuestionRepository;
 
 @SpringBootApplication
@@ -30,13 +31,21 @@ public class HhKyselyApplication {
 			TextQuestion q1 = new TextQuestion("Mikä on lempiruokasi?", esimerkkiKysely);
 			TextQuestion q2 = new TextQuestion("Mikä on lempijuomasi?", esimerkkiKysely);
 			RadioQuestion q3 = new RadioQuestion("Mikä on lempivärisi?", esimerkkiKysely);
-			q3.getChoices().add("A");
-			q3.getChoices().add("B");
-			q3.getChoices().add("C");
-			q3.getChoices().add("D");
+			q3.getChoices().add("Sininen");
+			q3.getChoices().add("Punainen");
+			q3.getChoices().add("Keltainen");
+			q3.getChoices().add("Vihreä");
+			CheckboxQuestion q4 = new CheckboxQuestion("Erikoisruokavaliotarpeet?", esimerkkiKysely);
+			q4.getChoices().add("Gluteenitot");
+			q4.getChoices().add("Maidoton");
+			q4.getChoices().add("Latkoositon");
+			q4.getChoices().add("Kasvis");
+			q4.getChoices().add("Vegaani");
+			q4.getChoices().add("Ei mikään");
 			questionRepository.save(q1);
 			questionRepository.save(q2);
 			questionRepository.save(q3);
+			questionRepository.save(q4);
 		};
 	}
 }
